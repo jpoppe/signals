22b5eb5410c720808fa8ded794a0f34d256c56a5
Jasper Poppe <jgpoppe@gmail.com>
2021-03-11 16:45:03 +0100

    hacking on azure

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2018 - 2021 Gemeente Amsterdam
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signals.settings.production')
-
-application = get_wsgi_application()
