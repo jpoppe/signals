6bde4dad1173e4eb28b09d666c0db5fa0e8fa180
Jasper Poppe <jgpoppe@gmail.com>
2021-03-12 08:27:00 +0100

    hacking on azure

--- scripts/azure-start-local.sh
+++ scripts/azure-start-local.sh
@@ -4,6 +4,6 @@
 # apt-get install -y sl gdal-bin python3-gdal libcairo2 python-gobject-2 libpango-1.0-0 libpangocairo-1.0-0 postgresql-client
 # /antenv/bin/python /home/site/wwwroot/api/app/manage.py collectstatic
 
-gunicorn --workers 2 --threads 2 --timeout 60 --access-logfile \
+/antenv/bin/gunicorn --workers 2 --threads 2 --timeout 60 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8001 \
      --chdir=/home/site/wwwroot/api/app signals.wsgi
