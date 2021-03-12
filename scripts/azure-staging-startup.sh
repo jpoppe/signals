f4dfe0b8dc1b4b17ac5b2b7b23e6a4fc885fb1ae
Jasper Poppe <jgpoppe@gmail.com>
2021-03-12 15:40:41 +0100

    hacking on azure

--- scripts/azure-staging-startup.sh
+++ scripts/azure-staging-startup.sh
@@ -3,10 +3,10 @@
 apt-get update
 apt-get install -y sl gdal-bin python3-gdal libcairo2 python-gobject-2 libpango-1.0-0 libpangocairo-1.0-0 postgresql-client
 
-# /antenv/bin/python /home/site/wwwroot/api/app/manage.py collectstatic
+python /home/site/wwwroot/api/app/manage.py collectstatic
 
 echo "🦄 starting gunicorn 🦄"
 
-gunicorn --workers 2 --threads 2 --timeout 60 --access-logfile \
-    '-' --error-logfile '-' --bind=0.0.0.0:8000 \
-     --chdir=/home/site/wwwroot/api/app signals.wsgi
+gunicorn --workers 2 --threads 2 --timeout 60 --access-logfile '/var/log/gunicorn-access.log' \
+  --error-logfile '/var/log/gunicorn-error.log' --bind=0.0.0.0:8000 \
+  --chdir=/home/site/wwwroot/api/app signals.wsgi
