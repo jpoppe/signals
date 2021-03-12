2b4b1fca20d3632810a52f964d83875953dde2a2
Jasper Poppe <jgpoppe@gmail.com>
2021-03-12 07:21:58 +0100

    hacking on azure

--- scripts/azure-staging-startup.sh
+++ scripts/azure-staging-startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y sl gdal-bin python3-gdal libcairo2 python-gobject-2 libpango-1.0-0 libpangocairo-1.0-0 postgresql-client
+# apt-get update
+# apt-get install -y sl gdal-bin python3-gdal libcairo2 python-gobject-2 libpango-1.0-0 libpangocairo-1.0-0 postgresql-client
+# /antenv/bin/python manage.py collectstatic
 
 gunicorn --workers 8 --threads 4 --timeout 60 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000 \
